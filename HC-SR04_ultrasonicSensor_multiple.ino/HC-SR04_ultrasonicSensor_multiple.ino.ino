20759ae8be8529cd11d5b527c736d74f5370c0f3
puneetsinghal <puneetatpks@gmail.com>
2017-01-25 12:18:20 -0500

    Add files via upload
    changed the max distance from 400 to 200...checked and running...

--- HC-SR04_ultrasonicSensor_multiple.ino/HC-SR04_ultrasonicSensor_multiple.ino.ino
+++ HC-SR04_ultrasonicSensor_multiple.ino/HC-SR04_ultrasonicSensor_multiple.ino.ino
@@ -20,7 +20,7 @@
     #include <NewPing.h>
      
     #define SONAR_NUM     3 // Number or sensors.
-    #define MAX_DISTANCE 400 // Max distance in cm.
+    #define MAX_DISTANCE 200 // Max distance in cm.
     #define PING_INTERVAL 33 // Milliseconds between pings.
      
     unsigned long pingTimer[SONAR_NUM]; // When each pings.
